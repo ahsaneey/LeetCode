bb0277c45f782e332b3d854dcdbd0a9b7ef93ce2
Ahsanamfirroz <148569197+ahsaneey@users.noreply.github.com>
2026-05-08 12:42:17 +0530

    Time: 2076 ms (30.16%) | Memory: 55 MB (50.82%) - LeetSync

---
+++ 507-perfect-number/perfect-number.js
@@ -0,0 +1,11 @@
+let num=28
+var checkPerfectNumber = function(num) {
+    let sum=0;
+    for(let i=1;i<num;i++){
+        if(num % i===0){
+            sum+=i
+        }
+    }
+    return sum===num;
+};
+console.log(checkPerfectNumber(num))
