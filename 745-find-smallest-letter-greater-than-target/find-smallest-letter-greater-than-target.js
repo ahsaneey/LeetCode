53b17086cc326ea5b0cf9dd06574e5446615c550
Ahsanamfirroz <148569197+ahsaneey@users.noreply.github.com>
2026-05-14 10:29:54 +0530

    Time: 0 ms (100.00%) | Memory: 56.4 MB (15.63%) - LeetSync

---
+++ 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.js
@@ -0,0 +1,12 @@
+let letters=["c","f","j"];
+let target="a"
+var nextGreatestLetter = function(letters, target) {
+    for(let i=0;i<letters.length;i++){
+        if(letters[i]>target){
+            return letters[i]
+        }
+    }
+    return letters[0]
+    
+};
+console.log(nextGreatestLetter(letters,target))
