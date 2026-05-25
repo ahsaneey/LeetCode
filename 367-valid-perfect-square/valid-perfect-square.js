a991d917df1bcf26619f741f68b4103c8fba800f
Ahsanamfirroz <148569197+ahsaneey@users.noreply.github.com>
2026-05-25 13:47:48 +0530

    Time: 2 ms (14.54%) | Memory: 54.7 MB (20.14%) - LeetSync

---
+++ 367-valid-perfect-square/valid-perfect-square.js
@@ -0,0 +1,13 @@
+let num=16;
+var isPerfectSquare = function(num) {
+    for(let i=1;i<=num;i++){
+        if(i*i===num){
+            return true;
+        }
+        if(i*i>num){
+            return false
+        }
+    }
+    
+}
+console.log(isPerfectSquare(num))
