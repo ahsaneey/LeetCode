a3fae49bb7fa946431be1d7961d80827af69f804
Ahsanamfirroz <148569197+ahsaneey@users.noreply.github.com>
2026-06-09 10:43:50 +0530

    Time: 1 ms (62.62%) | Memory: 57.6 MB (90.19%) - LeetSync

---
+++ 2048-build-array-from-permutation/build-array-from-permutation.js
@@ -0,0 +1,9 @@
+let nums=[0,2,1,5,3,4]
+var buildArray = function(nums) {
+    let result=[];
+    for(let i=0;i<nums.length;i++){
+        result.push(nums[nums[i]])
+    }
+    return result
+};
+console.log(buildArray(nums))
