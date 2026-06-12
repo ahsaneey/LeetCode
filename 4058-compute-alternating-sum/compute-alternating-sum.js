50bc8cb016a5a4b3883a273ecd19264a6c0000e9
Ahsanamfirroz <148569197+ahsaneey@users.noreply.github.com>
2026-06-12 12:13:45 +0530

    Time: 0 ms (100.00%) | Memory: 56.4 MB (46.91%) - LeetSync

---
+++ 4058-compute-alternating-sum/compute-alternating-sum.js
@@ -0,0 +1,14 @@
+let nums=[1,3,5,7]
+var alternatingSum = function(nums) {
+    let sum=0;
+    for(let i=0;i<nums.length;i++){
+        if(i%2===0){
+            sum+=nums[i]
+        }else{
+            sum-=nums[i]
+        }
+    }
+    return sum
+    
+}
+console.log(alternatingSum(nums))
