e6b34db69dbf231ef261e7002a14588186ec7912
Ahsanamfirroz <148569197+ahsaneey@users.noreply.github.com>
2026-06-26 10:20:25 +0530

    Time: 1 ms (83.50%) | Memory: 59.2 MB (11.15%) - LeetSync

---
+++ 3194-find-words-containing-character/find-words-containing-character.js
@@ -0,0 +1,14 @@
+let words= ["leet","code"];
+let x="e"
+var findWordsContaining = function(words, x) {
+    let result=[];
+    for(let i=0;i<words.length;i++){
+        if(words[i].includes(x)){
+            result.push(i)
+        }
+    }
+    return result
+    
+
+};
+console.log(findWordsContaining(words,x))
