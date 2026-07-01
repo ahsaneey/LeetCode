1058e2953a57b8a64f176057a88ea10adb90e164
Ahsanamfirroz <148569197+ahsaneey@users.noreply.github.com>
2026-07-01 19:59:45 +0530

    Time: 0 ms (100.00%) | Memory: 54.7 MB (91.80%) - LeetSync

---
+++ 3379-score-of-a-string/score-of-a-string.js
@@ -0,0 +1,10 @@
+let s="hello"
+var scoreOfString = function(s) {
+    let result=0;
+    for(let i=0;i<s.length-1;i++){
+        result+=Math.abs(s.charCodeAt(i) - s.charCodeAt(i+1))
+    }
+    return result;
+    
+}
+console.log(scoreOfString(s))
